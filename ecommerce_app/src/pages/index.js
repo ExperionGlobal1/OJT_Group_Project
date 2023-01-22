ef201cb6a27261815c50f4ac5524453f7eb65a40
Raj Kishore yadav <raj.yadav@experionglobal.com>
2023-01-22 11:18:58 +0530

    Product Listing Done

---
+++ ecommerce_app/src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "../pages/HomePage/HomePage";
+import Registration from "../pages/RegistrationPage/Registration";
+import  Verify  from "../pages/RegistrationPage/Verify";
+import Login from '../pages/LoginPage/Login';
+import CategoryProduct from "../pages/CategoryProductPage/CategoryProductPage";
+import Cart from "../pages/CartPage/CartPage";
+import ProductSingle from "./ProductSinglePage/ProductSinglePage";
+import Search from "./SearchPage/SearchPage";
+
+export {Home,CategoryProduct,Cart,ProductSingle,Registration,Verify,Login,Search};
