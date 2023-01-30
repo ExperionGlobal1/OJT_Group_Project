c2fb4a5222d2ba1e90d742dc7324388285a01767
Raj Kishore yadav <raj.yadav@experionglobal.com>
2023-01-30 12:40:24 +0530

    Updated CheckOutPage

--- ecommerce_app/src/pages/CheckOutPage/CheckOutPage.js
+++ ecommerce_app/src/pages/CheckOutPage/CheckOutPage.js
@@ -3,174 +3,120 @@ import { formatPrice } from '../../utils/helpers';
 import { getAllCarts } from '../../store/cartSlice';
 import { useSelector } from 'react-redux';
 import { useState } from "react";
+import { Link ,useNavigate} from "react-router-dom";
+import Header from "../../components/Header/Header";
 
 
 export default function CheckOutPage() {
-    const [cardDetails, setCardDetails] = useState(false);
-    const [upi, setUpiDetails] = useState(false);
-    const [net, setNetDetails] = useState(false);
-    const [message, setMessage] = useState();
-    const [error, setError] = useState(false);
+    // const [cardDetails, setCardDetails] = useState(false);
+    // const [upi, setUpiDetails] = useState(false);
+    // const [net, setNetDetails] = useState(false);
+    // const [message, setMessage] = useState();
+    // const [error, setError] = useState(false);
+    const nav =useNavigate();
+    const [isDisabled, setDisabled] = useState(false);
+
     const clickHandleSubmit = () => {
         alert("Completed Successfully..!!")
+        nav('/payment')
+
+    }
+    const handleDisable = () => {
+        console.log('Your button was clicked and is now disabled');
+        setDisabled(true);
     }
     const carts = useSelector(getAllCarts);
     const { itemsCount, totalAmount } = useSelector((state) => state.cart);
 
-    const handleCard = () => {
-        if (cardDetails == false) {
-            setCardDetails(true);
-            setUpiDetails(false);
-            setNetDetails(false);
-        }
-        else {
-            setCardDetails(false);
-        }
-
-    }
-    const handleUpi = () => {
-        if (upi == false) {
-            setUpiDetails(true);
-            setCardDetails(false);
-            setNetDetails(false);
-        }
-        else {
-            setUpiDetails(false);
-        }
-
-    }
-    const handleNet = () => {
-        if (net == false) {
-            setNetDetails(true);
-            setCardDetails(false);
-            setUpiDetails(false);
-        }
-        else {
-            setNetDetails(false);
-        }
-
-    }
-   
     return (
-        <div className="checkoutt">
-            <form>
-                <div className="data-side">
-                    <div className="details">
-                        <div className="form-groups">
-                            <label htmlFor="firstname" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">First Name</h6></label>
-                            <input id="firstname" className="mb-4" type="text" name="firstname" placeholder="Enter First name" required></input>
-                        </div>
-                        <div className="form-groups">
-                            <label htmlFor="lastname" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Last Name</h6></label>
-                            <input id="lastname" className="mb-4" type="text" name="lastname" placeholder="Enter Second name" required></input>
-                        </div>
-                        <div className="form-groups">
-                            <label htmlFor="phn" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Mobile Number</h6></label>
-                            <input id="phn" className="mb-4" type="number" name="phn" required></input>
-                        </div>
-                        <div className="address">
-                            <label htmlFor="address" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Address</h6></label>
-                            <input id="fulladdress" className="fulladdress" type="text" name="fulladdress" placeholder="enter your full address..." required></input>
-                        </div>
-                        <div className="side-data">
-                            <div className="pincode">
-                                <label htmlFor="pin" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">PIN CODE</h6></label>
-                                <input id="pin" className="mb-4" type="number" name="pin" required></input>
+        <>
+        <Header/>
+            <div className="checkoutt-page">
+
+                <form onSubmit={clickHandleSubmit}>
+                    <h2 class="text-center">Deliver to:</h2>
+                    <div className="data-side">
+
+                        <div className="details">
+
+                            <div className="form-groups-check">
+                                <label htmlFor="firstname" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">First Name</h6></label>
+                                <input id="firstname" className="mb-4" type="text" name="firstname" placeholder="Enter first name" required></input>
                             </div>
-                            <div className="state">
-                                <label htmlFor="state" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">State</h6></label>
-                                <select id="state" className="mb-4" type="text" name="state" required>
-                                    <option>select </option>
-                                    <option>Kerala </option>
-                                    <option>Tamil Nadu</option>
-                                    <option>Karnataka</option>
-                                    <option>Goa</option>
-
-                                </select>
+                            <div className="form-groups-check">
+                                <label htmlFor="lastname" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Last Name</h6></label>
+                                <input id="lastname" className="mb-4" type="text" name="lastname" placeholder="Enter last name" required></input>
                             </div>
-                        </div>
-                        <button className="btn btn-default bttn-proceed">proceed to pay</button>
-                    </div>
-
-                    <div className='cart-cbody bg-white checkout-body'>
-                        <div className='total-txt flex align-center justify-end'>
-                            <div className='font-manrope fw-5'>Total ({itemsCount}) items: </div>
-                            <span className='text-orange fs-22 mx-2 fw-6'>{formatPrice(totalAmount)}</span>
-                        </div>
-
-                        <div className="payment">
-                            <label htmlFor="pin">Net Banking</label>
-
-                            <input type="radio" value="option1" name="check" onChange={handleNet} ></input>
-
-                            <label htmlFor="upi">UPI</label>
-                            <input type="radio" value="option2" name="check" onChange={handleUpi} ></input>
-
-                            <label htmlFor="card">Card</label>
-                            <input type="radio" value="option3" name="check" onChange={handleCard}></input>
-
-                        </div>
-                        {cardDetails &&
-                            <div>
-                                <div className="form-groups">
-                                    <label htmlFor="cardnum" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Card Number</h6></label>
-                                    <input id="cardnum" className="mb-4" type="number" name="cardnum" placeholder="0000 0000 0000 0000"></input>
+                            <div className="form-groups-check">
+                                <label htmlFor="phn" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Mobile Number</h6></label>
+                                <input id="phn" className="mb-4" type="number" name="phn" placeholder="Enter Mobile No" required></input>
+                            </div>
+                            <br></br>
+                            <div className="address">
+                                <label htmlFor="address" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Address</h6></label>
+                                <input id="fulladdress" className="fulladdress" type="text" name="fulladdress" placeholder="Enter your full address..." required></input>
+                            </div>
+                            <br></br>
+                            <div className="side-data">
+                                <div className="pincd">
+                                    <label htmlFor="pin" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">PIN CODE</h6></label>
+                                    <input id="pin" className="mb-4" type="number" name="pin" placeholder="Enter pin code"required></input>
                                 </div>
-                                <div className="pincode">
-                                    <label htmlFor="cvv" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">CVV</h6></label>
-                                    <input id="cvv" className="mb-4" type="text" name="cvv" maxLength="3" placeholder="123" ></input>
-                                </div>
-                                <div className="form-groups">
-                                    <label htmlFor="holder" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Holder Name</h6></label>
-                                    <input id="holder" className="mb-4" type="text" name="holder" placeholder="Enter card holder name" ></input>
-                                </div>
-                                <div className="pincode">
-                                    <label htmlFor="expirydate" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Expiry Date</h6></label>
-                                    <input id="expirydate" className="mb-4" type="date" name="expirydate" ></input>
-                                </div>
-                            </div >
-                        }
-                        {upi &&
-                            <div>
-                                <div className="form-groups">
-                                    <label htmlFor="holder" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">UPI ID</h6></label>
-                                    <input id="upiid" className="mb-4" type="text" name="upiid" placeholder="abc@oksbi" required></input>
+                                <div className="state">
+                                    <label htmlFor="state" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">State</h6></label>
+                                    <select id="state" className="mb-4" type="text" name="state" required>
+                                        <option>select </option>
+                                        <option>Kerala </option>
+                                        <option>Tamil Nadu</option>
+                                        <option>Karnataka</option>
+                                        <option>Goa</option>
+                                        <option>Bihar</option>
+
+                                    </select>
                                 </div>
                             </div>
-                        }
-                        {net &&
-                            <div>
-                                <div className="form-groups">
-                                    <label htmlFor="bank" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Bank Name</h6></label>
-                                    <input id="bank" className="mb-4" type="text" name="bank" placeholder="type here..."></input>
-                                </div>
-                                <div className="form-groups">
-                                    <label htmlFor="acount" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Account Number</h6></label>
-                                    <input id="acount" className="mb-4" type="number" name="acount" placeholder="type here..."></input>
-                                </div>
-                                <div className="form-groups">
-                                    <label htmlFor="ifsc" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">IFSC</h6></label>
-                                    <input id="ifsc" className="mb-4" type="number" name="ifsc" placeholder="type here..."></input>
-                                </div>
 
-                                <div className="form-groups">
-                                    <label htmlFor="branch" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Branch</h6></label>
-                                    <input id="branch" className="mb-4" type="text" name="branch" placeholder="type here..."></input>
-                                </div>
+                            {/* <Link to="/payment"> */}
+                                <button className="btn btn-default bttn-proceed" style={{marginTop:"50px",marginLeft:"280px"}} >Proceed to Pay</button>
+                            {/* </Link> */}
 
+                        </div>
+                        <div className="box">
+                            {
+                                carts.map((cart, idx) => {
+                                    return (
+                                        <div className='cart-ctr py-4' key={cart?.id}>
+                                            <div className='cart-one'>
+                                                <span className='cart-ctxt'>{cart?.title}</span>
+                                            </div>
+
+                                            <div className='cart-one'>
+                                                <span className='cart-ctxt text-orange fw-5'>{formatPrice(cart?.totalPrice)}</span>
+                                            </div>
+
+
+                                        </div>
+                                    )
+                                }
+                                )
+                            }
+
+                            <div className='cart-cbody bg-white checkout-body'>
+                                <div className='total-txt flex align-center justify-end'>
+                                    <div className='font-manrope fw-5'>Total ({itemsCount}) items: </div>
+                                    <span className='text-orange fs-22 mx-2 fw-6'>{formatPrice(totalAmount)}</span>
+                                </div>
                             </div>
+                        </div>
 
-                        }
 
                     </div>
 
-                </div>
-                <div className="final-submit">
-                    <button classname="btn btn-default btn-checkout" type="submit" onSubmit={clickHandleSubmit}>Submit</button>
-                </div>
-            </form>
 
-        </div>
+                </form>
+
+            </div>
+        </>
 
     )
 
---
+++ ecommerce_app/src/pages/CheckOutPage/PaymentPage.js
@@ -0,0 +1,180 @@
+import "./PaymentPage.css";
+import { formatPrice } from '../../utils/helpers';
+import { getAllCarts } from '../../store/cartSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Header from '../../components/Header/Header';
+import { removeFromCart } from '../../store/cartSlice';
+
+
+
+const CheckOutPage = () => {
+    
+    const [cardDetails, setCardDetails] = useState(false);
+    const [upi, setUpiDetails] = useState(false);
+    const [net, setNetDetails] = useState(false);
+    // const [message, setMessage] = useState();
+    // const [error, setError] = useState(false);
+    const [isDisabled, setDisabled] = useState(false);
+    
+    const carts = useSelector(getAllCarts);
+    const { itemsCount, totalAmount } = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
+    
+
+    const nav = useNavigate();
+
+    const clickHandleSubmit = () => {
+        carts.map((cart) => {
+            dispatch(removeFromCart(cart?.id))
+        })
+        alert("Payment Done..!!")
+        nav('/')
+    }
+    const handleDisable = () => {
+        console.log('Your button was clicked and is now disabled');
+        setDisabled(true);
+    }
+
+    const handleCard = () => {
+        if (cardDetails == false) {
+            setCardDetails(true);
+            setUpiDetails(false);
+            setNetDetails(false);
+        }
+        else {
+            setCardDetails(false);
+        }
+
+    }
+    const handleUpi = () => {
+        if (upi == false) {
+            setUpiDetails(true);
+            setCardDetails(false);
+            setNetDetails(false);
+        }
+        else {
+            setUpiDetails(false);
+        }
+
+    }
+    const handleNet = () => {
+        if (net == false) {
+            setNetDetails(true);
+            setCardDetails(false);
+            setUpiDetails(false);
+        }
+        else {
+            setNetDetails(false);
+        }
+
+    }
+
+    return (
+        <div className="payments">
+            <Header />
+            <div className="checkoutt">
+                <form onSubmit={clickHandleSubmit}>
+                    <div className="data-side">
+
+                        <div className='cart-cbody bg-white checkout-body' >
+                            <div className='total-txt flex align-center justify-end'>
+                                <div className='font-manrope fw-5'>Total ({itemsCount}) items: </div>
+                                <span className='text-orange fs-22 mx-2 fw-6'>{formatPrice(totalAmount)}</span>
+                            </div>
+
+                            <div className="payment">
+                                <label htmlFor="pin">Net Banking</label>
+
+                                <input type="radio" value="option1" name="check" onChange={handleNet} required ></input>
+
+                                <label htmlFor="upi">UPI</label>
+                                <input type="radio" value="option2" name="check" onChange={handleUpi} required ></input>
+
+                                <label htmlFor="card">Card</label>
+                                <input type="radio" value="option3" name="check" onChange={handleCard} required></input>
+
+                            </div>
+                            {cardDetails &&
+                                <div>
+                                    <div className="form-groups">
+                                        <label htmlFor="holder" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Holder Name</h6></label>
+                                        <input id="holder" className="mb-4" type="text" name="holder" placeholder="Enter card holder name" required></input>
+                                    </div>
+                                    <div className="form-groups">
+                                        <label htmlFor="cardnum" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Card Number</h6></label>
+                                        <input id="cardnum" className="mb-4" type="number" name="cardnum" placeholder="0000 0000 0000 0000" required></input>
+                                    </div>
+                                    <div className="pincode">
+                                        <label htmlFor="cvv" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">CVV</h6></label>
+                                        <input id="cvv" className="mb-4" type="text" name="cvv" maxLength="3" placeholder="123" required></input>
+                                    </div>
+                                    
+                                    <div className="pincode">
+                                        <label htmlFor="expirydate" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Expiry Date</h6></label>
+                                        <input id="expirydate" className="mb-4" type="date" name="expirydate" required></input>
+                                    </div>
+                                </div >
+                            }
+                            {upi &&
+                                <div>
+                                    <div className="form-groups">
+                                        <label htmlFor="holder" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">UPI ID</h6></label>
+                                        <input id="upiid" className="mb-4" type="text" name="upiid" placeholder="abc@oksbi" required></input>
+                                    </div>
+                                </div>
+                            }
+                            {net &&
+                                <div>
+                                    <div className="form-groups">
+                                        <label htmlFor="bank" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Bank Name</h6></label>
+                                        <input id="bank" className="mb-4" type="text" name="bank" placeholder="type here..." required></input>
+                                    </div>
+                                    <div className="form-groups">
+                                        <label htmlFor="acount" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Account Number</h6></label>
+                                        <input id="acount" className="mb-4" type="number" name="acount" placeholder="type here..." required></input>
+                                    </div>
+                                    <div className="form-groups">
+                                        <label htmlFor="ifsc" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">IFSC</h6></label>
+                                        <input id="ifsc" className="mb-4" type="number" name="ifsc" placeholder="type here..." required></input>
+                                    </div>
+
+                                    <div className="form-groups">
+                                        <label htmlFor="branch" className="mb-1"><h6 className="mb-0 text-sm" id="lab12">Branch</h6></label>
+                                        <input id="branch" className="mb-4" type="text" name="branch" placeholder="type here..." required></input>
+                                    </div>
+
+                                </div>
+
+                            }
+
+                        </div>
+
+                    </div>
+                    <div className="final-submit" >
+                        <div>
+                            <Link to="/checkout">
+                                <button classname="btn btn-default btn-checkout" type="submit" style={{ marginLeft: "10px", backgroundcolor: "black", color: "white", width: "auto", height: "45px" }} >Back </button>
+                            </Link>
+                        </div>
+                        <div>
+
+                            <button classname="btn btn-default btn-checkout" type="submit" style={{ width: "100px", height: "45px", color: "white" }} >Submit</button>
+                        </div>
+
+
+
+
+                    </div>
+
+
+                </form>
+
+            </div>
+        </div>
+
+    );
+
+};
+export default CheckOutPage;
